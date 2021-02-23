03c6da8c3269ed87ce4c4872d317a96030fc5cc0
yuy.hirohiro@gmail.com <yuy.hirohiro@gmail.com>
2021-02-23 22:46:27 +0900

    add gockサンプル

--- gock-sample/gock_test.go
+++ gock-sample/gock_test.go
@@ -19,17 +19,23 @@ func TestName(t *testing.T) {
 
 	gock.New(MOCK_URL).
 		Get(PING_PATH).
-		MatchHeader("x-sample", "1.0").
+		MatchHeader("x-api-version", "1.[0-9]+").
 		Reply(200).
 		JSON(map[string]string{"foo": "bar"})
-
+	gock.New(MOCK_URL).
+		Get("/aaaa").
+		Reply(200).
+		JSON(map[string]string{"foo": "bar"})
 	//res, err := http.Get("http://" + MOCK_URL + PING_PATH)
 	//handleError(err)
-	req, err := http.NewRequest("GET", "http://"+MOCK_URL+PING_PATH, nil)
+	req, err := http.NewRequest(http.MethodGet, "http://"+MOCK_URL+PING_PATH, nil)
 	handleError(err)
-	req.Header.Set("x-sample", "1.0")
 
-	res, err := (&http.Client{}).Do(req)
+	req.Header = map[string][]string{"x-api-version": {"1.0"}}
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+
 	handleError(err)
 
 	bodyByte, err := io.ReadAll(res.Body)
@@ -37,6 +43,7 @@ func TestName(t *testing.T) {
 
 	assert.Equal(t, 200, res.StatusCode)
 	assert.Equal(t, `{"foo":"bar"}`, string(bodyByte)[:13])
+	assert.Equal(t, gock.IsDone(), true)
 }
 
 func handleError(err error) {
